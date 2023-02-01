d39d4c3e38f5ad4d58b5dc07a8ce74d2a7e0d13b
Binyam Tafesse <tafessebinyam51@gmail.com>
2023-01-31 18:47:08 -0600

    second commit

---
+++ mysql.java/src/main/java/recipies/Recipies.java
@@ -0,0 +1,13 @@
+package recipies;
+
+import recipies.dao.DbConnection;
+
+public class Recipies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+
+	}
+
+}
---
+++ mysql.java/src/main/java/recipies/dao/DbConnection.java
@@ -0,0 +1,30 @@
+package recipies.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import recipies.exceptions.DbException;
+
+public class DbConnection {
+	private static final String SCHEMA = "recipies";
+	private static final  String  USER = "recipies";
+	private static final String PASSWORD = "recipies";
+	private static final String HOST = "localhost";
+	private static final int PORT =3306;
+	
+public static Connection getConnection () {
+	String url= String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",HOST,PORT,SCHEMA
+			,USER,PASSWORD);
+	
+	System.out.println("connecting with url "+ url);
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("succesfuly obtained connection");
+			return conn;
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	
+}
+}
